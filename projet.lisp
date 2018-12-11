21aea44529a98bb7b263e7534e2a8eb8b567b951
aly nayef <alynayef12@gmail.com>
2018-12-11 17:06:53 +0100

    maj

--- projet.lisp
+++ projet.lisp
@@ -12,7 +12,7 @@
     nom
     matieres
     )
-    (setq Francais (make-matiere :nom "Francais"
+    (cons Francais (make-matiere :nom "Francais"
                                  :moyenne 10   ) )
     (setq Philosophie (make-matiere :nom "Philosophie"
                                  :moyenne 12   ) )
@@ -28,12 +28,14 @@
                                  :moyenne 15   ) )
     (setq SVT (make-matiere :nom "SVT"
                                  :moyenne 10   ) ) 
+
     (setq Litteraire '(Francais Philosophie Anglais))
     (setq Economie '(Economie Histoire Mathematiques))
     (setq Scientifique '(Mathematiques Physique SVT))
-    (setq notes '(Francais Philosophie Anglais Economie Histoire Mathematiques Physique SVT))
+
+    (setq notes  '(Francais Philosophie Anglais Economie Histoire Mathematiques Physique SVT))
     ( setq eleve1 (make-eleve :nom "nathanael"
      :matieres notes )
 ) 
-
 (print eleve1)
+
