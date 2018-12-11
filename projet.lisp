b4a310e32c433c9e7e8c1b1c4aebf37ff8a91869
aly nayef <alynayef12@gmail.com>
2018-12-11 16:55:01 +0100

    structure complete

--- projet.lisp
+++ projet.lisp
@@ -1,9 +1,39 @@
+(defstruct matiere
+    nom
+    moyenne
+    )
+
+(defstruct filiere
+    nom
+    matieres
+    )
+
 (defstruct eleve
     nom
-    matiere
+    matieres
     )
-    (setq matieres '(10 12 8 20 5 4 15 10) )
+    (setq Francais (make-matiere :nom "Francais"
+                                 :moyenne 10   ) )
+    (setq Philosophie (make-matiere :nom "Philosophie"
+                                 :moyenne 12   ) )
+    (setq Anglais (make-matiere :nom "Anglais"
+                                 :moyenne 8   ) )
+    (setq Economie (make-matiere :nom "Economie"
+                                 :moyenne 20   ) )
+    (setq Histoire (make-matiere :nom "Histoire"
+                                 :moyenne 5   ) )
+    (setq Mathematiques (make-matiere :nom "Mathematiques"
+                                 :moyenne 4   ) )
+    (setq Physique (make-matiere :nom "Physique"
+                                 :moyenne 15   ) )
+    (setq SVT (make-matiere :nom "SVT"
+                                 :moyenne 10   ) ) 
+    (setq Litteraire '(Francais Philosophie Anglais))
+    (setq Economie '(Economie Histoire Mathematiques))
+    (setq Scientifique '(Mathematiques Physique SVT))
+    (setq notes '(Francais Philosophie Anglais Economie Histoire Mathematiques Physique SVT))
     ( setq eleve1 (make-eleve :nom "nathanael"
-     :matiere matieres )
+     :matieres notes )
 ) 
+
 (print eleve1)
