8b9d42979d98aa1a8ea4852b074033dfdc38f6ab
aly nayef <alynayef12@gmail.com>
2018-12-11 16:35:46 +0100

    structure

--- projet.lisp
+++ projet.lisp
@@ -1,1 +1,9 @@
-
+(defstruct eleve
+    nom
+    matiere
+    )
+    (setq matieres '(10 12 8 20 5 4 15 10) )
+    ( setq eleve1 (make-eleve :nom "nathanael"
+     :matiere matieres )
+) 
+(print eleve1)
