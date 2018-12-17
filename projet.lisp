073d0c5363804fdedfce0796a04c885b7557cff6
aly nayef <alynayef12@gmail.com>
2018-12-17 09:16:17 +0100

    add structure list

--- projet.lisp
+++ projet.lisp
@@ -4,39 +4,49 @@
     )
 
 (defstruct filiere
-    nom
-    matieres
+    nomfiliere
+    matiere
+    moyennefiliere
     )
 
 (defstruct eleve
     nom
-    matieres
+    resultats
     )
-    (setq Francais (make-matiere :nom "Francais"
-                                 :moyenne 10   ) )
-    (setq Philosophie (make-matiere :nom "Philosophie"
-                                 :moyenne 12   ) )
-    (setq Anglais (make-matiere :nom "Anglais"
-                                 :moyenne 8   ) )
-    (setq Economie (make-matiere :nom "Economie"
-                                 :moyenne 20   ) )
-    (setq Histoire (make-matiere :nom "Histoire"
-                                 :moyenne 5   ) )
-    (setq Mathematiques (make-matiere :nom "Mathematiques"
-                                 :moyenne 4   ) )
-    (setq Physique (make-matiere :nom "Physique"
-                                 :moyenne 15   ) )
-    (setq SVT (make-matiere :nom "SVT"
-                                 :moyenne 10   ) ) 
+    (setq Francais (make-matiere :nom 'Francais
+                                 :moyenne 10.0   ) )
+    (setq Philosophie (make-matiere :nom 'Philosophie
+                                 :moyenne 12.0   ) )
+    (setq Anglais (make-matiere :nom 'Anglais
+                                 :moyenne 8.0  ) )
+    (setq Economie (make-matiere :nom 'Economie
+                                 :moyenne 20.0   ) )
+    (setq Histoire (make-matiere :nom 'Histoire
+                                 :moyenne 5.0   ) )
+    (setq Mathematiques (make-matiere :nom 'Mathematiques
+                                 :moyenne 4.0   ) )
+    (setq Physique (make-matiere :nom 'Physique
+                                 :moyenne 15.0   ) )
+    (setq SVT (make-matiere :nom 'SVT
+                                 :moyenne 10.0   ) ) 
 
-    (setq Litteraire '(Francais Philosophie Anglais))
-    (setq Economie '(Economie Histoire Mathematiques))
-    (setq Scientifique '(Mathematiques Physique SVT))
+    (setq sommeMoyenne (+ ( matiere-moyenne Francais) ( matiere-moyenne Philosophie) ( matiere-moyenne Anglais)))
+    (setq moyenneGeneral (/ sommeMoyenne 3))
+    (setq Litteraire (make-filiere :nomfiliere 'Litteraire :matiere (list Francais Philosophie Anglais) :moyennefiliere moyenneGeneral))
 
-    (setq notes  '(Francais Philosophie Anglais Economie Histoire Mathematiques Physique SVT))
-    ( setq eleve1 (make-eleve :nom "nathanael"
-     :matieres notes )
+    (setq sommeMoyenne (+ ( matiere-moyenne Economie) ( matiere-moyenne Histoire) ( matiere-moyenne Mathematiques)))
+    (setq moyenneGeneral (/ sommeMoyenne 3))
+    (setq Economie (make-filiere :nomfiliere 'Economie :matiere (list Economie Histoire Mathematiques) :moyennefiliere moyenneGeneral))
+
+    (setq sommeMoyenne (+ ( matiere-moyenne Mathematiques) ( matiere-moyenne Physique) ( matiere-moyenne SVT)))
+    (setq moyenneGeneral (/ sommeMoyenne 3))
+    (setq Scientifique (make-filiere :nomfiliere 'Scientifique :matiere (list Mathematiques Physique SVT) :moyennefiliere moyenneGeneral))
+
+    ( setq eleve1 (make-eleve :nom 'nathanael
+     :resultats (list Litteraire Economie Scientifique ))
 )
-(print Litteraire) 
+
 (print eleve1)
- (setq notes  '(Francais Philosophie Anglais Economie Histoire Mathematiques Physique SVT))
+
+ 
+ 
