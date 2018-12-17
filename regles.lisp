ab9c9e967988b6b5a049d8009dc0c53b3de5d207
aly nayef <alynayef12@gmail.com>
2018-12-17 10:42:57 +0100

    add function moyenne

--- regles.lisp
+++ regles.lisp
@@ -2,7 +2,7 @@
 ;---------------------------------
 
 (defun calcule-moyennes (eleve)
-      ((setq eleve.noteL (/ (+ eleve.Philosophie.moyenne eleve.Français.moyenne eleve.Anglais.moyenne) 3))
+      ((setq eleve.noteL (/ (+ eleve.Philosophie.moyenne eleve.Franï¿½ais.moyenne eleve.Anglais.moyenne) 3))
        (setq eleve.noteES (/ (+ eleve.Mathematiques.moyenne eleve.Economie.moyenne eleve.Histoire.moyenne) 3))
        (setq eleve.noteS (/ (+ eleve.Mathematiques.moyenne eleve.Physique.moyenne eleve.SVT.moyenne) 3))))
 
@@ -11,9 +11,9 @@
 
 (defun R2-R3 (eleve)
        (cond ((and (< eleve.noteL 10) (< eleve.noteS 10) (< eleve.noteES 10)) (print "Un redoublement serait bien"))
-             (t (cond ((> noteL 9.99) (format t "Filière littéraire conseillée à ~d" (/ noteL (calcul-ponderation-total noteL noteES noteS))))
-                      ((> noteES 9.99) (format t "Filière economie et Social conseillée à ~d" (/ noteES (calcul-ponderation-total noteL noteES noteS))))
-                      ((> noteS 9.99) (format t "Filière scientifique conseillée à ~d" (/ noteS (calcul-ponderation-total noteL noteES noteS))))))))
+             (t (cond ((> noteL 9.99) (format t "Filiere litteraire conseillee a ~d" (/ noteL (calcul-ponderation-total noteL noteES noteS))))
+                      ((> noteES 9.99) (format t "Filiere economie et Social conseillee a  ~d" (/ noteES (calcul-ponderation-total noteL noteES noteS))))
+                      ((> noteS 9.99) (format t "Filiere scientifique conseillee a ~d" (/ noteS (calcul-ponderation-total noteL noteES noteS))))))))
 
 (defun calcul-ponderation-note (note)
        (cond ((> note 9.99) note)
