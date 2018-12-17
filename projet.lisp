97e65f92157f31889a043423a7b1026e3f37e8b5
aly nayef <alynayef12@gmail.com>
2018-12-17 10:45:19 +0100

    change function moyenneGeneral

--- projet.lisp
+++ projet.lisp
@@ -37,19 +37,14 @@
     (write "saisir la moyenne en SVT: ")
     (setq SVT (make-matiere :nom 'SVT
                                  :moyenne (read)   ) ) 
-    (defun moyenneGeneral (note1 note2 note3) (/ (+ (+ note1 note2) note3) 3))
+    (defun moyenneGeneral (note1 note2 note3) (/ (+  note1 note2 note3) 3))
 
-    (setq sommeLitteraire  (+ ( matiere-moyenne Francais) ( matiere-moyenne Philosophie) ( matiere-moyenne Anglais)))
-    (setq moyenneLitteraire (/ sommeLitteraire 3))
+
     (setq  moyenneLitteraire (moyenneGeneral ( matiere-moyenne Francais) ( matiere-moyenne Philosophie) ( matiere-moyenne Anglais)))
     (setq Litteraire (make-filiere :nomfiliere 'Litteraire :matiere (list Francais Philosophie Anglais) :moyennefiliere moyenneLitteraire))
 
-    (setq sommeEconomie (+ ( matiere-moyenne Economie) ( matiere-moyenne Histoire) ( matiere-moyenne Mathematiques)))
-    (setq moyenneEconomie (/ sommeEconomie 3))
     (setq Economie (make-filiere :nomfiliere 'Economie :matiere (list Economie Histoire Mathematiques) :moyennefiliere -1))
 
-    (setq sommeScientifique (+ ( matiere-moyenne Mathematiques) ( matiere-moyenne Physique) ( matiere-moyenne SVT)))
-    (setq moyenneScientifique (/ sommeScientifique 3))
     (setq Scientifique (make-filiere :nomfiliere 'Scientifique :matiere (list Mathematiques Physique SVT) :moyennefiliere -1))
 
     ( setq eleve1 (make-eleve :nom 'nathanael
