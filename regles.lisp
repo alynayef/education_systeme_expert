5b9bf8722a0c1f481dd196f382c7ff29055b878c
Dyanek <nathanael.omnes@gmail.com>
2018-12-17 09:09:21 +0100

    Définition des règles, paramètres à changer en fonction de comment on choisit une variable

--- regles.lisp
+++ regles.lisp
@@ -11,4 +11,13 @@
 
 (defun R2-R3 (eleve)
        (cond ((and (< eleve.noteL 10) (< eleve.noteS 10) (< eleve.noteES 10)) (print "Un redoublement serait bien"))
-             (t (cond ((> eleve.noteL 10) )))
+             (t (cond ((> noteL 9.99) (format t "Filière littéraire conseillée à ~d" (/ noteL (calcul-ponderation-total noteL noteES noteS))))
+                      ((> noteES 9.99) (format t "Filière economie et Social conseillée à ~d" (/ noteES (calcul-ponderation-total noteL noteES noteS))))
+                      ((> noteS 9.99) (format t "Filière scientifique conseillée à ~d" (/ noteS (calcul-ponderation-total noteL noteES noteS))))))))
+
+(defun calcul-ponderation-note (note)
+       (cond ((> note 9.99) note)
+             (t 0)))
+
+(defun calcul-ponderation-total (noteL noteES noteS)
+       (+ (calcul-ponderation-note noteL) (calcul-ponderation-note noteES) (calcul-ponderation-note noteS)))
