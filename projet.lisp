7e96a0bf61937848c132e07bfd9c8d53088e251e
Aly Nayef Basma <alynayef12@gmail.com>
2018-12-21 13:35:25 +0100

    recu

--- projet.lisp
+++ projet.lisp
@@ -74,22 +74,22 @@
 
 (setq regle1 (make-regle :nom 'regle1
                          :utilisee 'nil
-                         :condition '(or (equal ( filiere-moyennefiliere Litteraire) -1) (equal ( filiere-moyennefiliere Economie) -1.0) (equal ( filiere-moyennefiliere Scientifique) -1.0))
+                         :condition '(or (equal ( filiere-moyennefiliere Litteraire) -1) (equal ( filiere-moyennefiliere FEconomie) -1.0) (equal ( filiere-moyennefiliere Scientifique) -1.0))
                          :action '((setf ( filiere-moyennefiliere Litteraire) (moyenneGenerale ( matiere-moyenne Philosophie) ( matiere-moyenne Anglais) ( matiere-moyenne Francais)))
                                    (setf ( filiere-moyennefiliere Scientifique) (moyenneGenerale ( matiere-moyenne Mathematiques) ( matiere-moyenne Physique) ( matiere-moyenne SVT)))
-                                   (setf ( filiere-moyennefiliere Economie) (moyenneGenerale ( matiere-moyenne Mathematiques) ( matiere-moyenne Economie) ( matiere-moyenne Histoire))))))
+                                   (setf ( filiere-moyennefiliere FEconomie) (moyenneGenerale ( matiere-moyenne Mathematiques) ( matiere-moyenne Economie) ( matiere-moyenne Histoire))))))
 
 (setq regle2 (make-regle :nom 'regle2
                          :utilisee 'nil
-                         :condition '(and (< ( filiere-moyennefiliere Litteraire) 10) (< ( filiere-moyennefiliere Economie) 10) (< ( filiere-moyennefiliere Scientifique) 10)
+                         :condition '(and (< ( filiere-moyennefiliere Litteraire) 10) (< ( filiere-moyennefiliere FEconomie) 10) (< ( filiere-moyennefiliere Scientifique) 10)
                          :action '(printf "Un redoublement serait bien!"))))
 
 (setq regle3 (make-regle :nom 'regle3
                          :utilisee 'nil
-                         :condition '(or (> ( filiere-moyennefiliere Litteraire) 9.99) (> ( filiere-moyennefiliere Economie) 9.99) (> ( filiere-moyennefiliere Scientifique) 9.99))
-                         :action '(cond ((> noteL 9.99) (format t "Filiere litteraire conseillee ~d" (/ noteL (calcul-ponderation-total ( filiere-moyennefiliere Litteraire) ( filiere-moyennefiliere Economie)( filiere-moyennefiliere Scientifique)))))
-                                        ((> noteES 9.99) (format t "Filiere economie et Social conseillee ~d" (/ noteES (calcul-ponderation-total ( filiere-moyennefiliere Litteraire) ( filiere-moyennefiliere Economie)  ( filiere-moyennefiliere Scientifique)))))
-                                        ((> noteS 9.99) (format t "Filiere scientifique conseillee ~d" (/ noteS (calcul-ponderation-total ( filiere-moyennefiliere Litteraire) ( filiere-moyennefiliere Economie) ( filiere-moyennefiliere Scientifique))))))))
+                         :condition '(or (> ( filiere-moyennefiliere Litteraire) 9.99) (> ( filiere-moyennefiliere FEconomie) 9.99) (> ( filiere-moyennefiliere Scientifique) 9.99))
+                         :action '(cond ((> noteL 9.99) (format t "Filiere litteraire conseillee ~d" (/ noteL (calcul-ponderation-total ( filiere-moyennefiliere Litteraire) ( filiere-moyennefiliere FEconomie) ( filiere-moyennefiliere Scientifique)))))
+                                        ((> noteES 9.99) (format t "Filiere economie et Social conseillee ~d" (/ noteES (calcul-ponderation-total ( filiere-moyennefiliere Litteraire) ( filiere-moyennefiliere FEconomie)  ( filiere-moyennefiliere Scientifique)))))
+                                        ((> noteS 9.99) (format t "Filiere scientifique conseillee ~d" (/ noteS (calcul-ponderation-total ( filiere-moyennefiliere Litteraire) ( filiere-moyennefiliere FEconomie) ( filiere-moyennefiliere Scientifique))))))))
 
 (setq listeregle (list regle1 regle2 regle3))
 ;--------------------------
@@ -111,6 +111,7 @@
 (write-line "--------------------REGLES--------------------")
 (setq ponderationote (calcul-ponderation-total ( filiere-moyennefiliere Litteraire) ( filiere-moyennefiliere FEconomie) ( filiere-moyennefiliere Scientifique) ))
 (print ponderationote)
+(print (eval ( regle-condition regle1)) (eval ( regle-action regle1)))
 (write-line "-----------------MODIFICATION-----------------")
 (print eleve1)
 (setf  ( filiere-moyennefiliere Litteraire) (moyenneGenerale ( matiere-moyenne Francais) ( matiere-moyenne Philosophie) ( matiere-moyenne Anglais) ) )
